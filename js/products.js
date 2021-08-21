1627310269b9b9c5e957479508107e71ab10ef76
alentornsofia <alentorn.sofia@gmail.com>
2021-08-21 18:14:43 -0300

    Obligatorio 1

--- js/products.js
+++ js/products.js
@@ -3,4 +3,38 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    function cargarProductos(url) {
+
+        fetch(url)
+            .then(respuesta => respuesta.json())
+
+            .then(datos => {
+                for (i = 0; i < datos.length; i++) {
+
+                    let productos = document.getElementById("listaproductos");
+                    productos.innerHTML += `
+
+                
+                    <div class="row">
+                        <div class="col-3">
+                        <img src=" `+ datos[i].imgSrc +  `" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ datos[i].name +`</h4>
+                                
+                            </div>
+                            <p class="mb-1">Precio: U$S `+ datos[i].cost + ` </p>
+                            <p class="mb-1">` + datos[i].description + `</p>
+                        </div>
+                    </div>
+                
+                `
+
+                }
+                
+            });
+           
+    }
+    cargarProductos(PRODUCTS_URL);
 });
