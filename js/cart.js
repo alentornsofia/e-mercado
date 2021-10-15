3f81e5c2b40f9e37288f56d2ebfae17671c5fa23
alentornsofia <alentorn.sofia@gmail.com>
2021-10-15 17:34:56 -0300

    Obligatorio 5

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,92 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+
+function mostrarCarrito(productos) {
+    let htmlContentToAppend = "";
+    for (i = 0; i < productos.length; i++) {
+
+        let producto = productos[i];
+        let sub = producto.count*producto.unitCost;
+      
+            htmlContentToAppend += `
+           
+          
+
+          <tr>
+          <th scope="row"><img src=" `+ producto.src + `"  class="img-thumbnail""></th>
+          <td class="text-center"> ${producto.name}</td>
+          <td class="text-center"> <input type="number"  id="cantidad" onchange="calcularSubtotal(${producto.unitCost})"  value="${producto.count}"  min="1" max="10"></td>
+          <td class="text-center" id="productosubtotal">${producto.currency} ${sub}</td>
+        </tr> 
+
+      <tr>
+      <th scope="row">Tipo de envío:</th>
+      <td class="text-center"><strong>Premium</strong><br>(2-5 días)</td>
+      <td class="text-center">Costo de 15% sobre el subtotal</td>
+      <td class="text-center"><input class="form-check-input" type="radio" name="envio" id="envio1">
+      <label class="form-check-label" for="flexRadioDefault1"></td>
+    </tr> 
+    <tr>
+      <th scope="row"></th>
+      <td class="text-center"><strong>Express</strong><br>(5-8 días)</td>
+      <td class="text-center">Costo de 7% sobre el subtotal</td>
+      <td class="text-center"><input class="form-check-input" type="radio" name="envio" id="envio2">
+      <label class="form-check-label" for="flexRadioDefault1"></td>
+    </tr> 
+    <tr>
+    <th scope="row"></th>
+    <td class="text-center"><strong>Standard</strong><br>(12 a 15 días)</td>
+    <td class="text-center">Costo de 5% sobre el subtotal</td>
+    <td class="text-center"><input class="form-check-input" type="radio" name="envio" id="envio3">
+    <label class="form-check-label" for="flexRadioDefault1"></td>
+  </tr> 
+
+
+        <tr>
+        <th scope="row">Subtotal</th>
+        <td class="text-center"></td>
+        <td class="text-center"></td>
+        <td id="subtotal" class="text-center" >${producto.currency} ${sub}</td>
+      </tr>
+      <tr>
+        <th scope="row">Total</th>
+        <td class="text-center"></td>
+        <td class="text-center"></td>
+        <td id="total" class="text-center total" >${producto.currency} ${sub}</td>
+      </tr>
+
+      <tr>
+        <th >Envío</th>
+        <td class="text-center"></td>
+        <td class="text-center"></td>
+        <td id="total" class="text-center total" >${producto.currency} ${sub}</td>
+      </tr>
+      
+                `   
+    }
+        document.getElementById("carrito").innerHTML = htmlContentToAppend;
+        
+    }
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            
+            carrito = resultObj.data.articles
+            mostrarCarrito(carrito)
+
+        }
+    });
 });
+
+//CALCULO SUBTOTAL
+
+function calcularSubtotal (preciounitario) {
+    let cantidad = parseInt(document.getElementById("cantidad").value);
+    subtotal = preciounitario*cantidad;
+    document.getElementById("productosubtotal").innerHTML ="UYU " + subtotal;
+    document.getElementById("total").innerHTML = "UYU " + subtotal;
+    document.getElementById("subtotal").innerHTML = "UYU " + subtotal;
+
+}
